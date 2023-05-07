47633687a7c16396cb2fc51156ee03716b8d1eb6
Nick Felker <felker.nick@bk.ru>
2023-05-08 01:42:36 +0600

    refactor

---
+++ classes/connect.js
@@ -0,0 +1,31 @@
+import https from 'https';
+
+
+const Connect = {
+     get_connection(){
+        //  let flag = false;
+        // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+        // const response = fetch("https://rosreestr/api/desktop_app/get_connection.php", {
+        //     method: 'POST',
+        //     headers: {
+        //         "Access-Control-Allow-Origin": "*",
+        //         "Content-Type": "application/json; charset=UTF-8",
+        //
+        //     }
+        // })
+        //     .then((response)=>{
+        //         return response.json();
+        //     })
+        //     .then((data)=>{
+        //         if(data.ok){
+        //             flag = true;
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     })
+
+    }
+}
+
+export default Connect;
