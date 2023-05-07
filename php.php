1134efdceec88f479df20df877bc82b445d71a51
Nick Felker <felker.nick@bk.ru>
2023-05-07 19:17:31 +0600

    added base init functional

--- php.php
+++ php.php
@@ -1,4 +1,12 @@
 <?php
-echo "hello from php\n";
-echo var_dump($argv);
+    echo "hello from php\n";
+    echo var_dump($argv);
+
+    if($argv[1] == "init"){
+        mkdir("./.cfm");
+        $description_file_text = "Это текст описания вашего контейнера, отредактируйте этот файл, чтобы дать пользовательское описание";
+        $fp = fopen("./.cfm/description.txt", "w");
+        fwrite($fp, $description_file_text);
+        fclose($fp);
+    }
 ?>
